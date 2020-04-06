179c2780169b2b71ad6d77601c50e19ffaa22719
Leon <leon.karho.chung@gmail.com>
2020-04-06 15:36:00 -0400

    get transactions history when app mounts, filter history by currency, pass filtered data as props to respective currency route pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import CAD from "./pages/CAD";
+import BTC from "./pages/BTC";
+import ETH from "./pages/ETH";
 
 function App() {
-  return <div>hello world</div>;
+  const [loading, setLoading] = useState(true);
+  const [transactions, setTransactions] = useState([]);
+  const [currencies, setCurrencies] = useState({});
+  const getTransactionHistory = async () => {
+    const res = await fetch(
+      "https://shakepay.github.io/programming-exercise/web/transaction_history.json"
+    );
+    const data = await res.json();
+    setTransactions(data);
+    setLoading(false);
+  };
+
+  const FilterCurrencies = () => {
+    const currencies = {
+      CAD: [],
+      BTC: [],
+      ETH: [],
+    };
+    transactions.reduce((acc, current) => {
+      if (current.currency === "CAD") {
+        acc.CAD = [...acc.CAD, current];
+      } else if (current.currency === "BTC") {
+        acc.BTC = [...acc.BTC, current];
+      } else if (current.currency === "ETH") {
+        acc.ETH = [...acc.ETH, current];
+      }
+      return currencies;
+    }, currencies);
+    setCurrencies(currencies);
+  };
+
+  useEffect(() => {
+    getTransactionHistory();
+  }, []);
+
+  useEffect(() => {
+    FilterCurrencies();
+  }, [transactions]);
+
+  return loading ? (
+    <div>...loading...</div>
+  ) : (
+    <Router>
+      <nav>
+        <NavLink to="/cad">CAD</NavLink>
+        <NavLink to="btc">BTC</NavLink>
+        <NavLink to="eth">ETH</NavLink>
+      </nav>
+
+      <Route
+        path="/cad"
+        render={(props) => {
+          return <CAD {...props} data={currencies.CAD} />;
+        }}
+      />
+      <Route
+        path="/btc"
+        render={(props) => {
+          return <BTC {...props} data={currencies.BTC} />;
+        }}
+      />
+      <Route
+        path="/eth"
+        render={(props) => {
+          return <ETH {...props} data={currencies.ETH} />;
+        }}
+      />
+    </Router>
+  );
 }
 
 export default App;
